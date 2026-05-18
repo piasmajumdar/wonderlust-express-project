d1106a4152b7242170078c2058939e37de3aee9a
piasmajumdar <piasmajumdar@gmail.com>
2026-05-18 12:13:02 +0530

    Details Page Completed

--- src/app/destinations/[id]/page.jsx
+++ src/app/destinations/[id]/page.jsx
@@ -1,23 +1,27 @@
-import { Button } from '@heroui/react';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import { FaRegCalendar } from 'react-icons/fa';
-import { FiExternalLink } from 'react-icons/fi';
-import { LuMapPin } from 'react-icons/lu';
+import { descriptionVariants } from "@heroui/styles";
+import Image from "next/image";
+import Link from "next/link";
+import { FaRegCalendar } from "react-icons/fa";
+import { LuMapPin } from "react-icons/lu";
 
-const DestinationCard = ({ destination }) => {
-    const { _id, imageUrl, price, destinationName, duration, country } = destination;
+const DestinationDetailsPage = async ({ params }) => {
+    const { id } = await params;
+    // console.log(id)
 
+    const res = await fetch(`http://localhost:5000/destination/${id}`)
+    const destination = await res.json();
+    const { _id, imageUrl, price, destinationName, duration, country, description } = destination;
+
+    // console.log(destination)
     return (
-        <div className='border p-4 rounded-md'>
-            <figure className='h-[200px] overflow-hidden'>
+        <div className="max-w-7xl mx-auto my-10">
+            <figure className=''>
                 <Image
                     alt={destinationName}
                     src={imageUrl}
-                    height={600}
+                    height={200}
                     width={600}
-                    className='h-full bg-cover rounded-md'>
+                    className='h-[400px] w-auto mx-auto rounded-md'>
                 </Image>
             </figure>
 
@@ -39,12 +43,12 @@ const DestinationCard = ({ destination }) => {
                     </div>
                     <div><h3 className='text-2xl font-bold'>$ {price}</h3></div>
                 </div>
-                <Link href={`/destinations/${_id}`}>
-                    <Button variant='ghost' className={'mt-1 text-cyan-500'}><FiExternalLink /> Book Now</Button>
-                </Link>
+                <h1 className="mt-10 text-2xl">Overview</h1>
+                <p>{description}</p>
+
             </div>
         </div>
     );
 };
 
-export default DestinationCard;
+export default DestinationDetailsPage;
