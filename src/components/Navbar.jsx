8211fb60900f8e095bd99a50726e8ac494e8e9cb
piasmajumdar <piasmajumdar@gmail.com>
2026-05-18 09:30:35 +0530

    Navbar, Banner, Footer created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between bg-white p-5'>
+            <ul className='flex gap-3'>
+                <li><Link href={'/'}>Home</Link></li>
+                <li><Link href={'/destinations'}>Destination</Link></li>
+                <li><Link href={'/my-bookings'}>My Bookings</Link></li>
+            </ul>
+
+            <div>
+                <Image src={'/assets/Wanderlast.png'} alt='nav' height={150} width={150}></Image>
+            </div>
+            
+            <ul className='flex gap-3'>
+                <li><Link href={'/profile'}>Profie</Link></li>
+                <li><Link href={'/login'}>Login</Link></li>
+                <li><Link href={'/signup'}>Sign Up</Link></li>
+            </ul>
+            
+        </div>
+    );
+};
+
+export default Navbar;
